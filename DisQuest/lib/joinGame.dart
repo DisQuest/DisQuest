bbee3bb12e222d8e67eb5c8b8599f3c4772cb492
Shaheera Khalid <shaheera.khalid@mail.utoronto.cagit commit -mexitquitgit onfig --global user.email shaheera.khalid@mail.utoronto.ca>
2020-11-29 18:31:30 -0500

    fixed errors

--- DisQuest/lib/joinGame.dart
+++ DisQuest/lib/joinGame.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import "./addCheckpoint.dart";
-=======
 import 'flutterFire.dart';
-import './questsList.dart';
->>>>>>> firebase_setup
+//import './questsList.dart';
+import './addCheckpoint.dart';
 
 class JoinGame extends StatelessWidget {
   final usernameController = TextEditingController();
   final pinController = TextEditingController();
+  JoinGame({Key key, this.hostId, this.gameId}) : super(key: key);
+  final String hostId;
+  final String gameId;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,7 +45,7 @@ class JoinGame extends StatelessWidget {
                               MaterialPageRoute(
                                   // Note: I switched this
                                   builder: (context) => Material(
-                                      child: QuestsList(
+                                      child: CheckPoints(
                                           hostId: pinController.text,
                                           gameId: gameId)))),
                         }),

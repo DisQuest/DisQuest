23b8ee7437c30ae828aa8914a9b5a0ba65616f91
Shaheera Khalid <shaheera.khalid@mail.utoronto.cagit commit -mexitquitgit onfig --global user.email shaheera.khalid@mail.utoronto.ca>
2020-11-29 19:35:43 -0500

    sign up

---
+++ DisQuest/lib/signup_new.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SignUpNew extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "SIGNUP",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              press: () {},
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
